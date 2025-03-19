73ebd376277755da819575ffa7833b911b269b1e
Matthias Henning <matze_henning@gmx.de>
2025-03-19 20:21:39 +0100

    weiter

--- start/start.go
+++ start/start.go
@@ -1,9 +1,22 @@
 package main
 
 import "fmt"
+import "time"
 
 func main() {
+	var number int
+	fmt.Print("Enter a number: ")
+	for {
+		_, err := fmt.Scan(&number)
+		if err == nil {
+			break
+		}
+		fmt.Print("Invalid input. Please enter a number: ")
+		
+	}
+	
 	for range 10 {
-		fmt.Println("Hello, World!")
+		time.Sleep(20 * time.Millisecond)
+		fmt.Printf("Hello, World! %d\n", number)
 	}
 }
