bdfaabaeb69e95fbd6b4525414427fa989aa0e63
Matthias Henning <henning@geomarketing.de>
2025-03-26 14:26:05 +0100

    weiter

--- start/start.go
+++ start/start.go
@@ -3,7 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	for range 10 {
-		fmt.Println("Hello, World!")
+	for  {
+		fmt.Print("Enter a number: ")
+		var input int
+		count, err := fmt.Scan(&input)
+		if err != nil || count != 1 {
+			fmt.Println("Invalid input, please enter ONE number.")
+		}else{
+			fmt.Printf("You entered: %d", input)
+			break
+		}
 	}
 }
